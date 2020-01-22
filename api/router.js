5f6d2679cc4c4b51e6bd12af9c7c77f2d5eb3235
Ariana Shackelford <ariana.shackelford@gmail.com>
2020-01-22 10:04:19 -0500

    finished crud operations

--- api/router.js
+++ api/router.js
@@ -6,64 +6,99 @@ const router = express.Router();
 
 //create new account
 
-router.post('/', async (req, res, next) => {
-try{
-    const payload = {
-        name : req.body.name,
-        budget : req.body.budget
-    }
-    const [id] = await db("accounts").insert(payload);
-    res.json(await db("accounts").where("id", id).select())
-} catch(err) {
-    next(err)
-}
-})
+router.post('/',(req, res) => {
+   const accountData = req.body;
+    
+    db("accounts")
+    .insert(accountData)
+    .then(newId => {
+        db("accounts")
+        .where({ id : newId[0]})
+        .then(newAccount => {
+            res.status(201).json(newAccount)
+        })
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({message: "We ran into an issue creating that account"})
+    });
+    
+    })
 
 //get all accounts
 
-router.get('/', async (req, res, next) => {
-    try{
-        res.json(await db("accounts").select());
-
-    } catch(err) {
-        next(err);
-    }
+router.get('/', (req, res, next) => {
+    db("accounts")
+    .then(accounts => {
+        res.json(accounts)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({message: "we ran into an issue fetching accounts"})
+    })
 })
 
 //get account by id
 
-router.get('/:id', async (req, res, next) => {
-    try{
-       res.json(await db("accounts").where(id = req.params.id).select())
-    } catch(err) {
-        next(err);
-    }
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+
+    db("accounts").where({ id })
+    .then(accounts => {
+        if(accounts.length){
+            res.json(accounts)
+        }else{
+            res.status(404).json({message: "Could not find account with specified user id"})
+        }
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({message: "We ran into a problem fetching that account"})
+    })
 })
 
 //update account
 
-router.put('/:id', async (req, res, next) => {
-    try{
-        const payload = {
-            name : req.body.name,
-            budget : req.body.budget
-        }
-        const id = req.param.id
-        await db("accounts").where("id", req.params.id).update(payload);
-        res.json(await db("accounts").where("id", id).first())
-    } catch(err) {
-        next(err);
-    }
-})
+router.put('/:id', (req, res) => {
+    const accountUpdate = req.body;
+    
+    db("accounts")
+    .update(accountUpdate)
+    .then(id => {
+        db("accounts")
+        .where({ id : id[0]})
+        .then(updatedAccount => {
+            res.status(201).json(updatedAccount)
+        })
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({message: "We ran into an issue creating that account"})
+    });
+    
+    })
+
 
 //delete account
 
-router.delete('/', async (req, res, next) => {
-    try{
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    const changes = req.body;
 
-    } catch(err) {
-        next(err);
-    }
+    db("accounts")
+    .where({ id })
+    .delete(changes)
+    .then(count => {
+        if(count > 0){
+            res.status(201).json({message: `${count} record(s) deleted`})
+        }else{
+            res.status(404).json({message: "account could not be found"})
+        }
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({message: "There was an issue processing your request"})
+    })
 })
 
 module.exports = router;
